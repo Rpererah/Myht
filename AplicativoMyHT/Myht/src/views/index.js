c7ee47c962c06b016fc95ee18db875b44e006cc7
AutoGuResRaPeJu <rpererah@gmail.com>
2020-05-11 19:23:45 -0300

    arrumando nomes dos arquivos causando erro no index das views

--- AplicativoMyHT/Myht/src/views/index.js
+++ AplicativoMyHT/Myht/src/views/index.js
@@ -3,6 +3,6 @@ import Login from './login/Login';
 import Sobre from './sobre/Sobre';
 import Luzes from './luzes/Luzes';
 import Componentes from './mycomponents/Componentes';
-import Notificacao from './notificações/Notificacao';
+import Notificacao from './notificacoes/Notificacao';
 import Despertador from './despertador/Despertador';
 export {Home,Login,Sobre,Luzes,Componentes,Notificacao,Despertador};
